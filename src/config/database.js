39bcc89349a246bad072ecc8c923993f4a60e745
Eryk Scarabello Dias <erykscarabello97@gmail.com>
2020-08-21 19:58:31 -0300

    Alterando dados de acesso do db

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'root@123',
-    database: 'sqlnode',
+    host: 'ec2-107-20-104-234.compute-1.amazonaws.com',
+    username: 'odfiprahfwxqpj',
+    password: '40346c9db8009d3c013c88eeed5b7b562cb2d931bcd8b3056cf396e8b323d261',
+    database: 'd5vrtnbar5v4j9',
     define: {
         timestamps: true,
         underscored: true

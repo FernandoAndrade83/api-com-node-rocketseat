617f20afdba39efb87d05f2c0be220fee38cd5d3
Eryk Scarabello Dias <erykscarabello97@gmail.com>
2020-08-21 21:33:19 -0300

    Testando com a porta 3000

--- src/server.js
+++ src/server.js
@@ -9,6 +9,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(process.env.PORT || 3000, function(){
-console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(3000);

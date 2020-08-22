2d5e35b6e61d34b2c291bd6adef563d479e68472
Eryk Scarabello Dias <erykscarabello97@gmail.com>
2020-08-21 21:14:50 -0300

    Mais uma tentativa kkk

--- src/server.js
+++ src/server.js
@@ -9,4 +9,6 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(8080);
+app.listen(process.env.PORT || 3000, function(){
+console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
